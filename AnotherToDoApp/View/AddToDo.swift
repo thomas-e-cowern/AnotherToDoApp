ced27f093fcd9888f4e5fa69a08e523428fc6b7d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-17 05:37:58 -0500

    added add to do view

---
+++ AnotherToDoApp/View/AddToDo.swift
@@ -0,0 +1,25 @@
+//
+//  AddToDo.swift
+//  AnotherToDoApp
+//
+//  Created by Thomas Cowern New on 2/17/22.
+//
+
+import SwiftUI
+
+struct AddToDo: View {
+    
+    // MARK:  Properties
+    
+    // MARK:  Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK:  Preview
+struct AddToDo_Previews: PreviewProvider {
+    static var previews: some View {
+        AddToDo()
+    }
+}
