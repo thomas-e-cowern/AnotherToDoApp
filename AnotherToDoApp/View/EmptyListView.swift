0279d829359f3b4c436b11fd39d64a35b3b70218
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-17 14:59:35 -0500

    added random images and quotes to empty list view

--- AnotherToDoApp/View/EmptyListView.swift
+++ AnotherToDoApp/View/EmptyListView.swift
@@ -12,19 +12,21 @@ struct EmptyListView: View {
     // MARK:  Properties
     @State private var isAnimated: Bool = false
     
+    let images: [String]  = ["illustration-no1", "illustration-no2", "illustration-no3"]
     
+    let tips: [String] = ["Use your time wisely", "Keep it short and sweet", "Small is better", "Break it up in bite size pieces", "Don't overdue it", "Take a break when you need it", "Plan ahead to reduce stress"]
     
     // MARK:  Body
     var body: some View {
         ZStack {
             VStack (alignment: .center, spacing: 20) {
                 
-                Image("illustration-no1")
+                Image("\(images.randomElement() ?? self.images[0])")
                     .resizable()
                     .scaledToFit()
                     .frame(minWidth: 256, idealWidth: 280, maxWidth: 360, minHeight: 256, idealHeight: 280, maxHeight: 360, alignment: .center)
                     .layoutPriority(1)
-                Text("Use Your Time Wisely")
+                Text("\(tips.randomElement() ?? self.tips[0])")
                     .layoutPriority(0.5)
                     .font(.system(.headline, design: .rounded))
             } // End of VStack
