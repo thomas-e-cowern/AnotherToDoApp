d155dd8ca5902f11b55e37fcb805499885c96142
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-18 06:03:56 -0500

    added icon and text to form row static view

--- AnotherToDoApp/View/FormRowStaticView.swift
+++ AnotherToDoApp/View/FormRowStaticView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct FormRowStaticView: View {
+    
+    // MARK:  Properties
+    var icon: String
+    var firstText: String
+    var secondText: String
+    
+    // MARK:  Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(Color.gray)
+                Image(systemName: icon)
+                    .foregroundColor(Color.white)
+            }
+            .frame(width: 36, height: 36, alignment: .center)
+            
+            Text(firstText)
+                .foregroundColor(Color.gray)
+            
+            Spacer()
+            
+            Text(secondText)
+        }
     }
 }
 
+// MARK:  Preview
 struct FormRowStaticView_Previews: PreviewProvider {
     static var previews: some View {
-        FormRowStaticView()
+        FormRowStaticView(icon: "gear", firstText: "Application", secondText: "Another To Do")
+            .previewLayout(.fixed(width: 375, height: 60))
+            .padding()
     }
 }
