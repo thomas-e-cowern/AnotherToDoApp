2df9013ada3bf95cd63e04cab593481c496fa30c
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-18 05:35:21 -0500

    added settings view

---
+++ AnotherToDoApp/View/SettingsView.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsView.swift
+//  AnotherToDoApp
+//
+//  Created by Thomas Cowern New on 2/18/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    // MARK:  Properties
+    
+    
+    // MARK:  Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK:  Preview
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
